e390ed569d7b6a051556c4035fa5078cd0f778d3
yannick9letallec <yannick9letallec@gmail.com>
2019-03-06 21:16:07 +0100

    Every day tweet based on random card is OK. Need cron update on the server

--- app/libs/twitter.js
+++ app/libs/twitter.js
@@ -1,19 +1,77 @@
-const twit = require( 'twit' )
+const util = require( 'util' )
 const fs = require( 'fs' )
+const twit = require( 'twit' )
+const redis = require( 'redis' ).createClient() 
+
+redis.auth( 'Kixsell_1', function( err, reply ){
+	console.log( "REDIS AUTH : " + err ? err : reply ) 
+})
 
 let t = new twit({
-	consumer_key: 'KgMFIPkj0FnazE4XCXpJpEhL8',
-	consumer_secret: 'UYJ9g8NpNoSbSNjRLoeMoHvyIkWS6qz3euYge1r0vDduTIDqWK',
-	access_token: '1102710876967260161-Yu0AYqzCl3buBNBXdYcVhL93yg0dk7',
-	access_token_secret: 'YgzZ74CJ77ZL1eecKM0EwmeHIka87digoenG9CDtIHYRG',
-	timeout_ms: '60000',
+	consumer_key: 'Zf13qpjUpCATeTq25H7K09lLh',
+	consumer_secret: 'pKEzz12qy2NCtT5EVYfxMLj5wtMGJCXfpYV4S59PUACQMYaAww',
+	access_token: '1102710876967260161-ER1ORS01MhLXuNbtqyO5CKOYzzcVnX',
+	access_token_secret: 'vluWqqyr1pKzAeTJOYmWZXyhXRTxKesqh1U7Al4LzgrPt',
 	strictSSL: true
-
 })
 
 let img64 = fs.readFileSync( './img/rejouer.png', { encoding: 'base64' } )
 
-t.post( 'media/upload', { media_data: img64, name: 'rejouer',  media_type: 'image/png' }, function( err, data, response ){
+redis.llen( 'cartes', function( err, reply ){
+	if( err ) redisError( err )
+
+	console.log( "CARTES LIST + " + reply ) 
+	let n = reply,
+		r = Math.floor( Math.random() * reply )
+
+	redis.lindex( 'cartes', r, function( err, reply ){
+		if( err ) redisError( err )
+		
+		console.log( "CARTE CHOISIE : " ) 
+		console.log( reply) 
+
+		let nom_carte = reply
+
+		redis.hgetall( "Carte:" + reply, function( err, carte ){
+			if( err ) redisError( err )
+
+			console.dir( carte ) 
+			
+			let twitter_options = {
+				nom_carte,
+				carte: carte
+			}
+
+			let text = carte.text.slice( 0, 200 ),
+				status = `Tirage des #Ange. Nouvelle Carte: ${ nom_carte }. ${ text } ... https://messages-des-anges.fr/afficherTweet?carte=${ nom_carte }`
+				
+			// process.exit()
+			t.post( 'statuses/update', { status }, function( err, data, response ){
+				if(err) {
+					console.log( "TWEET ERROR SENDING STATUS : " + err )
+					return
+				}
+
+				console.log( "DATA ---------------------------------------" ) 
+				console.log( util.inspect( data, { depth: null, showProxy: true, showHidden: true } ) ) 
+
+				process.exit()
+			})
+
+		} )
+	})
+})
+
+function redisError( err ){
+	res.send( '[KO] REDIS ERROR ' + req.path )  
+	return console.log( "REDIS ERROR " + err ) 
+}
+
+/* 
+t.post( 'media/upload', { media_data: img64 }, function( err, data, response ){
+
+	console.log( "UPLOAD .... SIMPLE " ) 
+
 	if(err) {
 		console.log( "ERROR : " + err )
 		return
@@ -21,17 +79,54 @@ t.post( 'media/upload', { media_data: img64, name: 'rejouer',  media_type: 'imag
 
 
 	let mediaIdStr = data.media_id_string,
-		mediaId = data.media_id,
+		mediaId = data.media_id_string,
 		altText = 'img ...... to show in twitter'
-		meta_params = { media_id: mediaId, alt_text: { text: altText }, media_ids: [ mediaId, mediaIdStr ] }
+		meta_params = { media_id: mediaId, alt_text: { text: altText } }
 
+	console.log( "INIT" ) 
 	console.log( data )
 	console.log( meta_params ) 
 
-	console.log( "------------------------------------------------------------------------------------------------- LOADING MEDIA OK ..." ) 
 
+		let params = { status: '*****************## CE es amis est le aître ot ---', media_id: data.media_id, media_id_string: data.media_id_string }
+		console.log( params ) 
 
-	/*
+} )
+
+	t.get( 'search/tweets', { q: 'sexuality', count: 100 }, function( err, data, response ){
+		console.log( "SEARCH TWEETS" ) 
+
+		if(err) {
+			console.log( "ERROR 1 : " + err )
+			return
+		}
+
+		console.log( data ) 
+	})
+
+	t.get( 'media/upload', { command: 'STATUS', media_id: data.media_id_string }, function( err, data_s, response ){
+		console.log( "MEDIA UPLOAD STATUS " ) 
+
+		if(err) {
+			console.log( "ERROR 1 : " + err )
+			return
+		}
+
+		console.log( data_s ) 
+	})
+	t.post( 'media/upload', { segment_index: 0, media_id: mediaId, media_data: img64, command: 'APPEND' }, function( err, data, response ){
+
+		console.log( "UPLOAD .... APPEND " ) 
+
+		if(err) {
+			console.log( "ERROR 1 : " + err )
+			return
+		}
+
+		console.log( "APPEND" ) 
+		console.log( data )
+	})
+
 	process.exit()
 	t.post( 'media/metadata/create', meta_params, function( err, data, response ){
 		if(err) {
@@ -42,23 +137,5 @@ t.post( 'media/upload', { media_data: img64, name: 'rejouer',  media_type: 'imag
 		console.log( data ) 
 
 	} )
-	*/
-		let params = { status: '---- 0000000000000----', in_reply_to_status_id: 1102998847079485400, media_id: mediaId }
-		console.log( params ) 
+*/
 
-		t.post( 'statuses/update', params, function( err, data, response ){
-			if(err) {
-				console.log( "ERROR 2 : " + err )
-				return
-			}
-
-			console.log( "DATA ---------------------------------------" ) 
-			console.dir( data ) 
-
-			/*
-			console.log( "RESPONSE ---------------------------------------" ) 
-			console.dir( response ) 
-			*/
-
-		})
-} )
